b3b469942b68f6a0bce42016499bafe8dc52d98a
matthewfbenjamin <mbenjamin618@gmail.com>
2020-05-10 17:57:26 -0700

    Updates

---
+++ precache-manifest.244d33c422e7fb42e046bbf2ba869adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be789a9affcbf407056e31feb86f5597",
+    "url": "/react-spotify/index.html"
+  },
+  {
+    "revision": "c19e334b64604be65ee4",
+    "url": "/react-spotify/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b2e15b5d9d21997e5971",
+    "url": "/react-spotify/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c19e334b64604be65ee4",
+    "url": "/react-spotify/static/js/2.d7f55a65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-spotify/static/js/2.d7f55a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2e15b5d9d21997e5971",
+    "url": "/react-spotify/static/js/main.57b43c1f.chunk.js"
+  },
+  {
+    "revision": "24220529a90d61197ede",
+    "url": "/react-spotify/static/js/runtime-main.4ad0396e.js"
+  }
+]);
